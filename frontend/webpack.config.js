956563683efa4d3160b468c1cb5e754127828863
brunobc <brunobezerrachaves@gmail.com>
2019-03-25 10:51:21 -0300

    React todo app without redux

---
+++ frontend/webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './app.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public',
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules'
+        }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'app.css'
+        })
+    ],
+    module: {
+        rules: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: [
+                    '@babel/preset-env',
+                    '@babel/react'
+                ],
+                plugins: ['@babel/plugin-proposal-object-rest-spread']
+            }
+        }, {
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader'
+            ]
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+            loader: 'file-loader'
+        }]
+    }
+}
